5f985c626cce19b0c39721ca87840f7c6493f744
Andrew <murphyandrew.murphy>
2013-03-20 17:58:30 +0000

    Initial Commit

---
+++ CloudShareApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CloudShareApplication
+//
+//  Created by Andrew on 20/03/2013.
+//  Copyright (c) 2013 Andrew Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CloudShareApplication/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CloudShareApplication
+//
+//  Created by Andrew on 20/03/2013.
+//  Copyright (c) 2013 Andrew Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
